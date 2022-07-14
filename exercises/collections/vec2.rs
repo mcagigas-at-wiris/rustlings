27cb16011dae97a64755c3ecef306c01d1773f64
Manuel <manuel@wiris.com>
2022-07-14 16:44:31 +0200

    20220514 exercices

--- exercises/collections/vec2.rs
+++ exercises/collections/vec2.rs
@@ -7,13 +7,14 @@
 // Execute the command `rustlings hint vec2` if you need
 // hints.
 
-// I AM NOT DONE
 
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for i in v.iter_mut() {
-        // TODO: Fill this up so that each element in the Vec `v` is
-        // multiplied by 2.
+        *i *= 1;
+        
     }
+
+    v = v.iter().map(|x| x*2).collect(); // More elegant solution    
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
     v
--- exercises/strings/strings2.rs
+++ exercises/strings/strings2.rs
@@ -3,16 +3,30 @@
 // Execute `rustlings hint strings2` for hints :)
 
 // I AM NOT DONE
-
 fn main() {
     let word = String::from("green"); // Try not changing this line :)
-    if is_a_color_word(word) {
+    if is_a_color_word(&word) {
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
     }
+
+    //print_type_of("patata"); // Despite is_a_color_word signature expects a &str and &word is String 
+    // Rust is able "transform"  &String to &str ? 
+    let pata = "patata";
+    patata(pata);
+    print_type_of(pata);
+    println!("{pata}");
 }
 
 fn is_a_color_word(attempt: &str) -> bool {
     attempt == "green" || attempt == "blue" || attempt == "red"
+}
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+ 
+fn patata(st: &str) {
+    println!("{st}");
 }
